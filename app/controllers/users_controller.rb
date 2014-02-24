f92d6ccb3cb0f04b49319a04c1cb48c1af4d1e92
Daniel Kehoe <daniel@danielkehoe.com>
2014-02-24 12:33:15 -0800

    rails_apps_composer: add README files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+end
