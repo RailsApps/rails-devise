fc535874e423ee7584ba54daf3f1d15de3a8f43d
Daniel Kehoe <daniel@danielkehoe.com>
2014-02-24 12:33:23 -0800

    rails_apps_composer: set up database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Environment variables (ENV['...']) can be set in the file config/application.yml.
+# See http://railsapps.github.io/rails-environment-variables.html
+puts 'DEFAULT USERS'
+user = User.find_or_create_by_email :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'user: ' << user.email
