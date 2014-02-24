5602f6eb534a54c7f3f0cf8503a701006e0fbac4
Daniel Kehoe <daniel@danielkehoe.com>
2014-02-24 12:33:12 -0800

    rails_apps_composer: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+RailsDevise::Application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
