3e42e44fb46cb92762837f0047b6cbc8b478ab4d
PMDevelopment <paulmdevelopment@gmail.com>
2016-05-29 12:27:59 -0700

    Update devise_permitted_parameters.rb
    The "for" syntax is deprecated, using new "permit" syntax.

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,8 +8,8 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
 end
