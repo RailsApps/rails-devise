a69cb7453f50b40093d38fc1cf2ebdfb46f3b244
Roderick Monje <rod@foveacentral.com>
2018-10-01 17:40:23 -0400

    conf: disable mailer locally

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   # Send email in development mode?
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
 
 
   # Suppress logger output for asset requests.
